1a70b3c81fb3591ca20f03a94045c566265af835
nobel002 <43477230+nobel002@users.noreply.github.com>
2021-04-13 21:29:00 +0200

    Little bit of organisation Little rework in the video generator

--- VideoGenerator.py
+++ VideoGenerator.py
@@ -5,15 +5,23 @@ import PIL
 
 FRAMESIZE = (2000, 2000)
 FILENAME = "test001"
+PATH = "generated/image/*.png"
 def scaleAllImgs():
-  for imageObj in glob.glob('generated/*.png'):
+  for imageObj in glob.glob(PATH):
     image = Image.open(imageObj)
+    width, height = image.size
+    if width >= FRAMESIZE[0]:
+      FRAMESIZE = (width, FRAMESIZE[1])
+    if height >= FRAMESIZE[1]:
+      FRAMESIZE = (FRAMESIZE[0], height)
+
+  for imageObj in glob.glob(PATH):
     resized_image = image.resize(FRAMESIZE)
     resized_image = resized_image.save(imageObj)
 
-out = cv2.VideoWriter(f'generated/{FILENAME}.avi',cv2.VideoWriter_fourcc(*'DIVX'), 0.2, FRAMESIZE)
+out = cv2.VideoWriter(f'generated/videos/{FILENAME}.avi',cv2.VideoWriter_fourcc(*'DIVX'), 0.2, FRAMESIZE)
 
-for filename in glob.glob('generated/*.png'):
+for filename in glob.glob(PATH):
     img = cv2.imread(filename)
     out.write(img)
 
--- textExtractor.py
+++ textExtractor.py
@@ -4,7 +4,7 @@ import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = 'tesseract\\tesseract.exe'
 
-img = cv2.imread("generated\\Brutal.png")
+img = cv2.imread("generated\\images\\Brutal.png")
 
 text = pytesseract.image_to_string(image= img)
 
